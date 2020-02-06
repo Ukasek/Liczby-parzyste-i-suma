3b5e805381f8452fd10af2d782f4a592595cea29
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-02-06 11:45:47 +0100

    Liczby parzyste bez sumy// pytanie

---
+++ src/EvenNumbers.java
@@ -0,0 +1,11 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+
+        int summary = 0;
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+            // summary=
+        }
+        System.out.println("Suma liczb to: " + summary);
+    }
+}
